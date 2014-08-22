060feaeaa3a995dfc7c2d183ca5a459b3e16c37e
Freehaha <freehaha@gmail.com>
2014-08-22 14:10:10 +1000

    fix typo

--- account.go
+++ account.go
@@ -19,18 +19,18 @@ type Href struct {
 // email fields for accounts are unique within a directory and are used to log
 // into applications.
 type Account struct {
-	Href       string                     `json:"href,omitempty"`       // The resource fully qualified location URI
-	Username   string                     `json:"username,omitempty"`   // The username for the account. Must be unique across the owning directory. If not specified, the username will default to the email field.	1 < N <= 255 characters
-	Email      string                     `json:"email,omitempty"`      // The email address for the account. Must be unique across the owning directory.	1 < N <= 255 characters
-	Password   string                     `json:"password,omitempty"`   // The password for the account. Only include this property if setting or changing the account password.	1 < N <= 255 characters
-	GivenName  string                     `json:"givenName,omitempty"`  // The given (first) name for the account holder.	1 < N <= 255 characters
-	MiddleName string                     `json:"middleName,omitempty"` // The middle (second) name for the account holder.	1 < N <= 255 characters
-	Surname    string                     `json:"surname,omitempty"`    // The surname (last name) for the account holder.	1 < N <= 255 characters
-	Status     string                     `json:"status,omitempty"`     // Enabled accounts are able to login to their assigned applications.	Enum	enabled,disabled
-	Groups     Href                       `json:"groups,omitempty"`     // A link to the groups that the account belongs to.
-	Directory  Href                       `json:"directory,omitempty"`  // A link to the owning directory.
-	Tenant     Href                       `json:"tenant,omitempty"`     // A link to the tenant owning the directory the group belongs to.
-	CustomData map[string]json.RawMessage `json:"customData,omitempty"` // custom data
+	Href       string                      `json:"href,omitempty"`       // The resource fully qualified location URI
+	Username   string                      `json:"username,omitempty"`   // The username for the account. Must be unique across the owning directory. If not specified, the username will default to the email field.	1 < N <= 255 characters
+	Email      string                      `json:"email,omitempty"`      // The email address for the account. Must be unique across the owning directory.	1 < N <= 255 characters
+	Password   string                      `json:"password,omitempty"`   // The password for the account. Only include this property if setting or changing the account password.	1 < N <= 255 characters
+	GivenName  string                      `json:"givenName,omitempty"`  // The given (first) name for the account holder.	1 < N <= 255 characters
+	MiddleName string                      `json:"middleName,omitempty"` // The middle (second) name for the account holder.	1 < N <= 255 characters
+	Surname    string                      `json:"surname,omitempty"`    // The surname (last name) for the account holder.	1 < N <= 255 characters
+	Status     string                      `json:"status,omitempty"`     // Enabled accounts are able to login to their assigned applications.	Enum	enabled,disabled
+	Groups     Href                        `json:"groups,omitempty"`     // A link to the groups that the account belongs to.
+	Directory  Href                        `json:"directory,omitempty"`  // A link to the owning directory.
+	Tenant     Href                        `json:"tenant,omitempty"`     // A link to the tenant owning the directory the group belongs to.
+	CustomData map[string]*json.RawMessage `json:"customData,omitempty"` // custom data
 	app        *Application
 }
 
