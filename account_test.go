d8f78eadc5f923c2e84f8144fceae6ea55f7a303
John Sun <freehaha@gmail.com>
2014-09-03 18:41:54 +1000

    add tests for loading custom data

--- account_test.go
+++ account_test.go
@@ -3,6 +3,7 @@
 package stormpath
 
 import (
+	"encoding/json"
 	"github.com/bmizerany/assert"
 	"github.com/darkhelmet/env"
 	"github.com/jmcvetta/randutil"
@@ -91,4 +92,67 @@ func TestUpdateAccount(t *testing.T) {
 	acct1, _ := app.GetAccount(acct0.Href)
 	assert.Equal(t, "Mister", acct1.GivenName)
 	assert.Equal(t, "Spock", acct1.Surname)
+}
+
+func TestSaveCustomData(t *testing.T) {
+	app := setupApplication(t)
+	app.LoadCustomData = true
+	tmpl := createAccountTemplate(t)
+	acct0, _ := app.CreateAccount(tmpl)
+	defer acct0.Delete()
+	msg := json.RawMessage("\"foo\"")
+	acct0.CustomData = map[string]*json.RawMessage{
+		"field1": &msg,
+	}
+	err := acct0.Update()
+	if err != nil {
+		t.Fatal(err)
+	}
+	acct1, _ := app.GetAccount(acct0.Href)
+	resp := acct1.CustomData["field1"]
+	b, _ := resp.MarshalJSON()
+	s := string(b)
+	assert.Equal(t, "\"foo\"", s)
+}
+
+func TestDontLoadCustomData(t *testing.T) {
+	app := setupApplication(t)
+	tmpl := createAccountTemplate(t)
+	acct0, _ := app.CreateAccount(tmpl)
+	defer acct0.Delete()
+	msg := json.RawMessage("\"foo\"")
+	acct0.CustomData = map[string]*json.RawMessage{
+		"field1": &msg,
+	}
+	err := acct0.Update()
+	if err != nil {
+		t.Fatal(err)
+	}
+	acct1, _ := app.GetAccount(acct0.Href)
+	_, ok := acct1.CustomData["field1"]
+	assert.Equal(t, ok, false)
+}
+
+func TestManualLoadCustomData(t *testing.T) {
+	app := setupApplication(t)
+	tmpl := createAccountTemplate(t)
+	acct0, _ := app.CreateAccount(tmpl)
+	defer acct0.Delete()
+	msg := json.RawMessage("\"foo\"")
+	acct0.CustomData = map[string]*json.RawMessage{
+		"field1": &msg,
+	}
+	err := acct0.Update()
+	if err != nil {
+		t.Fatal(err)
+	}
+	acct1, _ := app.GetAccount(acct0.Href)
+	if err := acct1.LoadCustomData(); err != nil {
+		t.Fatal(err)
+	}
+	resp, ok := acct1.CustomData["field1"]
+	assert.Equal(t, ok, true)
+	b, _ := resp.MarshalJSON()
+	s := string(b)
+	assert.Equal(t, "\"foo\"", s)
 }
